4574c8f1b2a6e37640fb4ca616685a44985e50bc
AkilaNilamlgoda <akilanilmalgoda2@gmail.com>
2020-11-03 12:36:03 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, {useState} from "react";
+import './App.css';
+import ExpenseList from './components/ExpenseList'
+import ExpenseForm from './components/ExpenseForm'
+import Alert from './components/Alert'
+import { v4 as uuidv4 } from 'uuid';
+//import { useState } from 'react';
+
+export const initialExpenses = [
+  {id: uuidv4(), charge: "rent", amount: 1600},
+  {id: uuidv4(), charge: "car payment", amount: 1400},
+  {id: uuidv4(), charge: "credit card bill", amount: 200}
+];
+
+function App() {
+  //*****************state values***************/
+  //all expenses, add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  //single expense
+  const {charge, setCharge} = useState(" ");
+  //single amount
+  const {amount, setAmount} = useState(" ");
+  //*****************functionality***************/
+  const handleCharge =e=> {
+    setCharge(e.target.value)
+  };
+  const handleAmount=e=> {
+    setAmount(e.target.value)
+  };
+  const handleSubmit = e=> {
+    e.preventDefault();
+    if (charge !=="" && amount>0) {
+      const singleExpense = {id: uuidv4(), charge, amount};
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+    }
+  };
+
+  return (
+    <>
+      <Alert/>
+
+      <h1>Budget Calculator</h1>
+      <main className="App">
+
+      <ExpenseForm
+        charge={charge}
+        amount={amount}
+        handleAmount={handleAmount}
+        handleCharge={handleCharge}
+        handleSubmit={handleSubmit}/>
+
+      <ExpenseList 
+          expenses={expenses}/>
+      </main>
+      <h1>total spending:{" "}
+    <span className="Total">
+      $
+      {expenses.reduce((acc,curr) =>{
+        return (acc += curr.amount);
+      },0)}
+    </span>
+    </h1>  
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Alert = () => {
+    return (
+        <div>
+            This is the Alert
+        </div>
+    )
+}
+
+export default Alert;
